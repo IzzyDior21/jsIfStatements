d8b66f7c2a4308d7e9a60a5622b505476da9c37a
Izzy Dior <shop218916@gmail.com>
2025-02-22 11:47:14 -0600

    02/07/25 if Else Statements

---
+++ ifStatements.js
@@ -0,0 +1,16 @@
+// An IfElse statement is a conditional statment that js to make decision based on your code. Give options!!
+
+//if (condition) {
+//    // CODE FOR CONDITION
+//} else {
+//   // MORE CODE IF NEEDED
+//}
+
+
+let age = 18;
+
+if (age >= 18); {
+    console.log("You are and adult");
+} else {
+    console.log("You are a child");
+}
