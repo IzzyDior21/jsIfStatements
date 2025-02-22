b61d30f041b2375ed2140c8fc29c9866815f0a5e
Izzy Dior <shop218916@gmail.com>
2025-02-22 14:30:00 -0600

    02/07/25 IF & ELSE STATEMENTS

--- ifStatements.js
+++ ifStatements.js
@@ -1,16 +1,52 @@
-// An IfElse statement is a conditional statment that js to make decision based on your code. Give options!!
+// If | else statement is a conditional statment that allows js to make decisions based on your code: GIVES OPTINS
 
 //if (condition) {
 //    // CODE FOR CONDITION
 //} else {
-//   // MORE CODE IF NEEDED
+//    console.log("You are a child")
 //}
 
 
-let age = 18;
+//let age = 7;
+//
+//if (age >= 18) {
+//    console.log("You are an adult.")
+//} else {
+//    console.log("You are a child.")
+//}
 
-if (age >= 18); {
-    console.log("You are and adult");
+
+// GRADING SYSTEM
+
+//let grade = 55;
+//
+//if (grade >= 90) {
+//    console.log("You are an A student");    
+//} else if (grade >= 80) {
+//    console.log("You are a B student")
+//}  else if (grade >= 70) {
+//    console.log("You are a C student")
+//} else {
+//    console.log("You are a failing student")
+//}
+
+//  COMPAIRING AND LOGICAL OPERATERS 
+
+//> GREATER THAN
+//< LESSER THAN
+//>= GREATER THAN OR EQUAL To
+//<= LESSER THAN OR EQUAL To
+//!= DOES NOT EQUAL
+//== EQUALITY OPERATER 
+//=== STRICT EQUALITY
+//&& TRUE 
+//|| OR
+
+let isLoggedIn = true;
+let isAdmin = false;
+
+if (isLoggedIn || isAdmin) {
+    console.log("Welcome, Admin");    
 } else {
-    console.log("You are a child");
+    console.log("Access Denied");    
 }
